44f83b5ca7d4d6351658d804e863cbadf4d46c35
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2021-05-30 20:32:29 +0530

    Template Design

--- src/tasks/forms.py
+++ src/tasks/forms.py
@@ -4,6 +4,7 @@ from django.forms import ModelForm
 from .models import *  
 
 class TaskForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add new task...'}))
 
     class Meta:
         model = Task
